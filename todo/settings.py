b26014446c9c6c2ce32e96e66aefb7b0f59cb48e
Yukie M Billal <yukiembillal01@gmail.com>
2024-02-23 16:00:06 +0700

    chore: env secret key

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)(c9ick)c!dy3ji0)kcad&v+*%!*@*#ti4t^cqy2vnck1&2&=2'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
